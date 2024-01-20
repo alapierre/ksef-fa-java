a3ba8b75de941127dfd68526e62cf728a4b4bd91
Adrian Lapierre <al@alapierre.io>
2024-01-20 13:41:48 +0100

    project refactoring
    Split to 3 independent projects:
    - KSEF FA model
    - GOBL
    - GOBL to FA(2) converter

--- README.md
+++ README.md
@@ -4,9 +4,56 @@
 
 # KSeF FA Java model
 
-KSeF
+Model parser and validator for  XML FA(2) KSeF structure. 
 
-## Build Requirements
+# Sample
+
+## Parse and validate FA(2) to Java model
+
+````java
+FakturaSerializer serializer = new FakturaSerializer();
+Faktura invoice = serializer.fromFile(new File("src/test/resources/fa.xml"), true);
+````
+
+## Serialize model to XML (without validation)
+
+````java
+FakturaSerializer fakturaSerializer = new FakturaSerializer();
+
+Faktura invoice = fakturaSerializer.create();
+
+Faktura.Podmiot1 podmiot1 = new Faktura.Podmiot1();
+TAdres address = new TAdres();
+address.setAdresL1("ul. Kolejowa 123");
+address.setAdresL2("05-092 Łomianki");
+
+podmiot1.setAdres(address);
+invoice.setPodmiot1(podmiot1);
+
+val str = fakturaSerializer.toString(invoice, false);
+
+System.out.println(str);
+````
+
+# How to use with maven
+
+Stable project attracts are available in Maven Central, just add dependency to your project:
+
+````xml
+<dependency>
+    <groupId>io.alapierre.ksef.fa</groupId>
+    <artifactId>java-ksef-fa</artifactId>
+    <version>2.0.0</version>
+</dependency>
+````
+
+# Another connected GitGub Repositories
+
+- [gobl.java](https://github.com/alapierre/gobl.java) Java GOBL implementation
+- [gobl.java.ksef](https://github.com/alapierre/gobl.java.ksef) GOBL to FA(2) Converter
+
+
+# Build Requirements
 
 The project can be built on JDK17+.
 
--- ksef-fa-xml-model/src/test/java/io/alapierre/ksef/fa/model/xml/FakturaSerializerTest.java
+++ ksef-fa-xml-model/src/test/java/io/alapierre/ksef/fa/model/xml/FakturaSerializerTest.java
@@ -3,10 +3,10 @@ package io.alapierre.ksef.fa.model.xml;
 import lombok.val;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import pl.gov.crd.wzor._2023._06._29._12648.Faktura;
+import pl.gov.crd.wzor._2023._06._29._12648.TAdres;
 
 import java.io.File;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Adrian Lapierre {@literal al@alapierre.io}
@@ -15,14 +15,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class FakturaSerializerTest {
 
     @Test
-    void testSerialize() {
+    void parse() {
 
         FakturaSerializer serializer = new FakturaSerializer();
-        val invoice = serializer.fromFile(new File("src/test/resources/fa.xml"), true);
+        Faktura invoice = serializer.fromFile(new File("src/test/resources/fa.xml"), true);
 
         Assertions.assertNotNull(invoice);
         Assertions.assertNotNull(invoice.getNaglowek());
     }
 
+    @Test
+    void serialize() {
 
-}
+        FakturaSerializer fakturaSerializer = new FakturaSerializer();
+
+        Faktura invoice = fakturaSerializer.create();
+
+        Faktura.Podmiot1 podmiot1 = new Faktura.Podmiot1();
+        TAdres address = new TAdres();
+        address.setAdresL1("ul. Kolejowa 123");
+        address.setAdresL2("05-092 Łomianki");
+
+        podmiot1.setAdres(address);
+        invoice.setPodmiot1(podmiot1);
+
+        val str = fakturaSerializer.toString(invoice, false);
+
+        System.out.println(str);
+
+        Assertions.assertNotNull(str);
+
+    }
+
+
+
+    }
